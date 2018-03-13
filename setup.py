9d97e9481e62ec13aa8af2dbf92691f23fb9d833
Igor Korostil <eeghor@gmail.com>
2018-03-13 13:56:36 +1100

    updated docs

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ setup(name='gender',
       version='0.0.1',
       description='Get gender from name and email address',
       classifiers=[
-        'Programming Language :: Python :: 3.6.4'
-      ]
+      	'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6.4',
+        'Intended Audience :: Science/Research'
+      ],
       url='https://github.com/i9k/gender',
       author='Igor Korostil',
       author_email='eeghor@gmail.com',
       license='MIT',
-      install_requires=['json'],
-      packages=['funniest'],
+      install_requires=['json', 'unidecode'],
       include_package_data=True,
+      keywords='gender name email detection',
       zip_safe=False)
